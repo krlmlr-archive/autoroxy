ea05601588310050fda949418aa21e464b16bc04
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-06 00:27:02 +0200

    export, and enhance docs

--- R/autoroxy.R
+++ R/autoroxy.R
@@ -1,6 +1,9 @@
 #' Generate roxygen2 documentation during package build
 #'
 #' Add a call to this function to any file in your \code{R} directory.
+#' Use explicit qualification (like \code{autoroxy::autoroxy()}) to avoid
+#' importing the package and its dependencies.
+#' @export
 autoroxy <- function() {
   if ("DESCRIPTION" %in% dir()) {
     message("*** autoroxy: creating documentation")
