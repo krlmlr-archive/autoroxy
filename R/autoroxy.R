e2c04ab1ddb43030171dae539412a3f69aed0a21
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-06 00:15:59 +0200

    print message

--- R/autoroxy.R
+++ R/autoroxy.R
@@ -2,6 +2,8 @@
 #'
 #' Add a call to this function to any file in your \code{R} directory.
 autoroxy <- function() {
-  if ("DESCRIPTION" %in% dir())
+  if ("DESCRIPTION" %in% dir()) {
+    message("*** autoroxy: creating documentation")
     roxygen2::roxygenize()
+  }
 }
