b6108c2868d7f63e3bc55fbdf642ef9f4a777a40
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-16 20:17:53 +0200

    autoroxy from vignette kind of works

--- tests/testthat/test-autoroxy.R
+++ tests/testthat/test-autoroxy.R
@@ -4,11 +4,12 @@ test_that("can build package", {
   devtools::build("testDocumentation", quiet = TRUE)
   on.exit(unlink("testDocumentation_0.0-0.tar.gz"), add = TRUE)
 
-  expect_true(all(untar("testDocumentation_0.0-0.tar.gz", list = TRUE) != "man"))
+  tar_list <- untar("testDocumentation_0.0-0.tar.gz", list = TRUE)
+  expect_true("testDocumentation/man/" %in% tar_list)
+  expect_true("testDocumentation/man/dummy.Rd" %in% tar_list)
 })
 
 test_that("can check package", {
   on.exit(unlink("testDocumentation.Rcheck", recursive = TRUE), add = TRUE)
-  expect_error(devtools::check("testDocumentation", document = FALSE,
-                               check_dir = ".", quiet = TRUE))
+  devtools::check("testDocumentation", document = FALSE, check_dir = ".", quiet = TRUE)
 })
