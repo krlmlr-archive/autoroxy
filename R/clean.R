df9823b70c619f2d04cce1b27641e889c4b5fd79
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-16 20:47:40 +0200

    refactor

--- R/clean.R
+++ R/clean.R
@@ -1,5 +1,9 @@
 check_git_clean <- function(repo) {
-  if (length(unlist(git2r::status(repo))) != 0L) {
+  if (git_clean(repo)) {
     stop("Repository ", git2r::workdir(repo), " is not clean.", call. = FALSE)
   }
+}
+
+git_clean <- function(repo) {
+  length(unlist(git2r::status(repo))) == 0L)
 }
