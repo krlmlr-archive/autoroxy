d0dcf4407698789c081b37e5502c5a322ed16998
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-24 15:31:02 +0200

    rewrite all tests

---
+++ tests/testthat/helper-all.R
@@ -0,0 +1,10 @@
+create_temp_package <- function() {
+  test_dir <- tempfile("autoroxy-test", fileext = "dir")
+  dir.create(test_dir)
+  file.copy("testDocumentation", test_dir, recursive = TRUE)
+  pkg_path <- file.path(test_dir, "testDocumentation")
+  repo <- git2r::init(pkg_path)
+  git2r::add(repo, "*")
+  git2r::commit(repo, "initial")
+  repo
+}
