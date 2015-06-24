2dbd211f41853a4da1c554b87fa279b978140fbe
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-24 16:00:15 +0200

    oops

--- tests/testthat/helper-all.R
+++ tests/testthat/helper-all.R
@@ -4,8 +4,8 @@ create_temp_package <- function() {
   file.copy("testDocumentation", test_dir, recursive = TRUE)
   pkg_path <- file.path(test_dir, "testDocumentation")
   repo <- git2r::init(pkg_path)
+  git2r::config(repo, user.name="Alice", user.email="alice@example.org")
   git2r::add(repo, "*")
   git2r::commit(repo, "initial")
-  git2r::config(repo, user.name="Alice", user.email="alice@example.org")
   repo
 }
